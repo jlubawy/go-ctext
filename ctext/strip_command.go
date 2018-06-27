0c95e7402b62defe9c878a6e4195fb6f0ffd2548
Josh Lubawy <jlubawy@gmail.com>
2018-06-27 16:41:12 -0700

    Use new cli package

--- ctext/strip_command.go
+++ ctext/strip_command.go
@@ -9,63 +9,54 @@ import (
 	"io"
 	"os"
 
+	"github.com/jlubawy/go-cli"
 	"github.com/jlubawy/go-ctext"
 )
 
-const stripUsage = `usage: strip [-output output] [source file]
-Run 'ctext help strip' for details.
-`
+type StripOptions struct {
+	Output string
+}
 
-const stripHelp = `usage: ctext strip [-output output] [source file]
+var stripOptions StripOptions
 
-Strips comments from a C source file. If a file is not provided then the source
-is read from stdin.
-
-Options:
-
-	-output        file to output the stripped source to, or stdout if empty
-
-`
-
-var stripCommand = Command{
-	Name: "strip",
-	CmdFn: func(args []string) {
-		var flagOutput string
-
-		fs := flag.NewFlagSet("strip", flag.ExitOnError)
-		fs.Usage = func() { info(stripUsage) }
-		fs.StringVar(&flagOutput, "output", "", "file to output to, stdout if empty")
-		fs.Parse(args)
-
+var stripCommand = cli.Command{
+	Name:             "strip",
+	ShortDescription: "strip comments from a C source file",
+	Description: `Strips comments from a C source file. If a file is not provided then the source
+is read from stdin.`,
+	ShortUsage: "[-output output] [source file]",
+	SetupFlags: func(fs *flag.FlagSet) {
+		fs.StringVar(&stripOptions.Output, "output", "", "output file or stdout if empty")
+	},
+	Run: func(args []string) {
 		var r io.Reader
-		if fs.NArg() == 0 {
+		if len(args) == 0 {
 			r = os.Stdin
-		} else if fs.NArg() == 1 {
-			f, err := os.OpenFile(fs.Arg(0), os.O_RDONLY, 0664)
+		} else if len(args) == 1 {
+			f, err := os.OpenFile(args[0], os.O_RDONLY, 0664)
 			if err != nil {
-				fatalf("Error opening input file: %v\n", err)
+				cli.Fatalf("Error opening input file: %v\n", err)
 			}
 			defer f.Close()
 			r = f
 		} else {
-			fatal("Expected a single input file.\n")
+			cli.Fatal("Expected a single input file.\n")
 		}
 
 		var w io.Writer
-		if flagOutput == "" {
+		if stripOptions.Output == "" {
 			w = os.Stdout
 		} else {
-			f, err := os.OpenFile(flagOutput, os.O_CREATE|os.O_WRONLY, 0664)
+			f, err := os.OpenFile(stripOptions.Output, os.O_CREATE|os.O_WRONLY, 0664)
 			if err != nil {
-				fatalf("Error opening output file: %v\n", err)
+				cli.Fatalf("Error opening output file: %v\n", err)
 			}
 			defer f.Close()
 			w = f
 		}
 
 		if err := ctext.StripComments(w, r); err != nil {
-			fatalf("Error stripping comments: %v\n", err)
+			cli.Fatalf("Error stripping comments: %v\n", err)
 		}
 	},
-	HelpFn: func() { info(stripHelp) },
 }
